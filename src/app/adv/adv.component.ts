b9326fa11794b8cc262593d7316c4e110acc23f1
Ditto17 <ansh159@gmail.com>
2019-02-02 13:02:52 +0530

    First commit to project

---
+++ src/app/adv/adv.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adv',
+  templateUrl: './adv.component.html',
+  styleUrls: ['./adv.component.css']
+})
+export class AdvComponent implements OnInit {
+
+  constructor() { 
+    
+  
+      }
+  ngOnInit() {
+
+  // var c = 0; 
+  // var timer =;
+
+  // function adStart(){
+  //   var ad_imgs = document.getElementById("ad_img");
+  //     if (c < 2){
+  //       c++;}
+  //     else{
+  //       c = 1;  }
+  // }
+  // ad_imgs.src = "./../../assets/Images/"+c+".jpg";
+  // timer = setTimeout("adStart()",2000);
+  //}
+}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { OptionsComponent } from './options/options.component';
+import { AdvComponent } from './adv/adv.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OptionsComponent,
+    AdvComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
